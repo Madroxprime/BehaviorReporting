782e0223dc1a807d97a324423a09006912419d03
John Anderson <madroxprime@Gmail.com>
2017-09-24 13:49:04 -0500

    Set up filesystem, and init index resources

---
+++ main.js
@@ -0,0 +1,13 @@
+const electron = require('electron');
+const app = electron.app;
+
+const BrowserWindow = electron.BrowserWindow;
+
+var mainWindow;
+app.on('ready', function(){
+    mainWindow = new BrowserWindow({
+        width:1024,
+        height:768});
+
+        mainWindow.loadURL('file://'+__dirname+'/app/html/index.html');
+});
